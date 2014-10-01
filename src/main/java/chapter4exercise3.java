397b8abb21f5b7ccbc09c477f895f99aea27e7e6
unknown <grahamin@LHR10-HDB9VZ1.ant.amazon.com>
2014-10-01 12:07:42 +0100

    refactor some duplication

--- src/main/java/chapter4exercise3.java
+++ src/main/java/chapter4exercise3.java
@@ -1,14 +1,16 @@
 class chapter4exercise3 {
     private static void printBeers(final int numBeers) {
-        System.out.println(numBeers + getBottle(numBeers) + "of beer on the wall, "
-                           + numBeers + getBottle(numBeers) + "of beer. You take one down, pass it round, "
-                           + (numBeers-1) + getBottle(numBeers-1) + "of beer on the wall.");
+        String currentBottles = getBottle(numBeers);
+	String nextBottles = getBottle(numBeers - 1);
+        System.out.println(currentBottles + "of beer on the wall, "
+                           + currentBottles + "of beer. You take one down, pass it round, "
+                           + nextBottles + "of beer on the wall.");
         if (numBeers == 1) {return;}
         printBeers(numBeers-1);
     }
 
     private static String getBottle(final int count) {
-        return (count == 1 ? " bottle " : " bottles ");
+        return count + (count == 1 ? " bottle " : " bottles ");
     }
 
     public static void main(String[] args) {
